72d2e89b0b115f2d487badf98434ab040820d0e9
Marcin Witas <77293179+Wojtazzzz@users.noreply.github.com>
2023-02-15 13:17:36 +0100

    feat/add theme color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				theme: '#FDF8EE',
+			},
+		},
 	},
 	plugins: [],
 };
