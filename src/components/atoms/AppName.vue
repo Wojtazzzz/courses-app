08240ea3cf872161d286d6c64f78368ccaed3472
Marcin Witas <77293179+Wojtazzzz@users.noreply.github.com>
2023-02-15 13:17:50 +0100

    feat/add app name component

---
+++ src/components/atoms/AppName.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const NAME = 'Courses App';
+</script>
+
+<template>
+	<div class="flex items-center gap-2">
+		<img src="../icons/logo.svg" width="56" height="48" alt="" />
+		<h1 class="font-bold text-lg">{{ NAME }}</h1>
+	</div>
+</template>
