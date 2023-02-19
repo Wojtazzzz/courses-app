14c2d53e0ef509b84739f51921ffa77c500abfeb
Marcin Witas <77293179+Wojtazzzz@users.noreply.github.com>
2023-02-19 18:40:26 +0100

    refactor/add shadow to AccentContainer

--- src/components/atoms/AccentContainer.vue
+++ src/components/atoms/AccentContainer.vue
@@ -1,6 +1,6 @@
 <template>
 	<section
-		class="w-full md:w-11/12 mx-auto bg-primary rounded-xl py-8 lg:py-12 px-6 lg:px-8 relative"
+		class="w-full md:w-11/12 mx-auto bg-primary rounded-xl py-8 lg:py-12 shadow-md px-6 lg:px-8 relative"
 	>
 		<slot />
 	</section>
