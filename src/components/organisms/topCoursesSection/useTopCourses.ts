c862fc1299bd3b698205262d65f453b7169879a6
Marcin Witas <77293179+Wojtazzzz@users.noreply.github.com>
2023-02-21 19:55:37 +0100

    feat/add parsing response

--- src/components/organisms/topCoursesSection/useTopCourses.ts
+++ src/components/organisms/topCoursesSection/useTopCourses.ts
@@ -1,11 +1,24 @@
 import { useQuery } from '@tanstack/vue-query';
 import { axios } from '../../../utils/axios';
 import { getTopCoursesQK } from '../../../utils/queryKeys';
+import { z } from 'zod';
+
+const schema = z.array(
+	z.object({
+		id: z.number(),
+		name: z.string(),
+		price: z.number(),
+		thumbnail: z.string(),
+		sales: z.number().min(0),
+		rating: z.number().min(0).max(5),
+	})
+);
 
 const queryKey = getTopCoursesQK();
 
 const queryFn = async () => {
-	return await axios.get('/courses/bests').then((response) => response.data);
+	const response = await axios.get('/courses/bests');
+	return schema.parse(response.data);
 };
 
 export const useTopCourses = () => {
